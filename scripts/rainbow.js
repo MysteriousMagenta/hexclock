7e663866287c62bec6f7f362e36cfbd5450a0527
MysteriousMagenta <therealrebuilder@gmail.com>
2015-03-07 01:18:25 +0100

    Finished rainbow.js

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -9,10 +9,15 @@ var rainbow = [
 	"#8B00FF"
 ]
 
+function rainbowChange(index) {
+	if (index < rainbow.length) {
+		body.style["background-color"] = rainbow[index];
+		setTimeout(function() {rainbowChange(index-1)}, 250)
+	}
+}
+
 function backgroundRainbow() {
 	clearInterval()
-	for (var i=0; i < rainbow.length; i++) {
-		body.style["background-color"] = rainbow[i];
-	}
-	start()
+	rainbowChange(0);
+	setInterval(start(), seconds*1000);
 }
