fd4c1cf13d7a978c22e5cbe49b35c36c110a42a3
MysteriousMagenta <therealrebuilder@gmail.com>
2015-03-07 01:04:05 +0100

    Bug Squashing (Until further notice, they're all bug squashing)

--- scripts/time.js
+++ scripts/time.js
@@ -1,9 +1,9 @@
 
-
+var body = document.getElementsByTagName("body")[0];
 function getHexTime() {
 	var date = new Date();
 	var time = [date.getHours(), date.getMinutes(), date.getSeconds()]
-	var body = document.getElementsByTagName("body")[0];
+	//var body = document.getElementsByTagName("body")[0];
 	var timeString = "#";
 	for (var i=0; i < time.length; i++) {
 		var element = time[i].toString();
@@ -18,7 +18,7 @@ function getHexTime() {
 
 function displayColor() {
 	var colorTime = getHexTime();
-	var body = document.getElementsByTagName("body")[0];
+	//var body = document.getElementsByTagName("body")[0];
 	body.style["background-color"] = colorTime;
 	var colorHeader = document.getElementById("color");
 	colorHeader.innerText = colorTime;
@@ -26,11 +26,11 @@ function displayColor() {
 
 function start() {
 	var colorText = document.getElementById("color")
-	var body = document.getElementsByTagName("body")[0];
+	//var body = document.getElementsByTagName("body")[0];
 	if (!colorText) {
 		var colorHeader = document.createElement("h1");
 		colorHeader.id = "color";
-		body.addBefore(colorHeader, document.getElementById("explanation"))
+		body.insertBefore(colorHeader, document.getElementById("explanation"))
 	}
 	else {
 		if (colorText.style.display !== "block") {
