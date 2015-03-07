4a9252964cc5721a287ffd170876e7510ee38675
MysteriousMagenta <therealrebuilder@gmail.com>
2015-03-07 01:09:48 +0100

    Resorted to dark measures.

--- scripts/time.js
+++ scripts/time.js
@@ -3,7 +3,7 @@
 function getHexTime() {
 	var date = new Date();
 	var time = [date.getHours(), date.getMinutes(), date.getSeconds()]
-	var body = document.getElementsByTagName("body")[0];
+	var body = document.getElementById("body");
 	var timeString = "#";
 	for (var i=0; i < time.length; i++) {
 		var element = time[i].toString();
@@ -18,7 +18,7 @@ function getHexTime() {
 
 function displayColor() {
 	var colorTime = getHexTime();
-	var body = document.getElementsByTagName("body")[0];
+	var body = document.getElementById("body");
 	body.style["background-color"] = colorTime;
 	var colorHeader = document.getElementById("color");
 	colorHeader.innerText = colorTime;
@@ -26,8 +26,8 @@ function displayColor() {
 
 function start() {
 	var colorText = document.getElementById("color")
-	var body = document.getElementsByTagName("body")[0];
-	console.log(document.getElementsByTagName("body"), body);
+	var body = document.getElementById("body");
+	console.log(document.getElementById("body"), body);
 	if (!colorText) {
 		var colorHeader = document.createElement("h1");
 		colorHeader.id = "color";
